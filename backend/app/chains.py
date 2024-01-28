9ae463d283a028f5d4bdc0e73c6d9b78a10667c7
wjlee <wjlee.ling@gmail.com>
2024-01-28 22:14:45 +0900

    feat: conversational retrieval chain w/ retrieved sources

--- backend/app/chains.py
+++ backend/app/chains.py
@@ -1,10 +1,13 @@
 import chromadb
 
-from .templates import retrieval_prompt
+from .templates import retrieval_prompt, condense_question_prompt, combine_docs
+from operator import itemgetter
 from langchain_openai import ChatOpenAI
+from langchain.schema import format_document
+from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
@@ -17,18 +20,50 @@ vectorstore = Chroma(
     collection_name="test-0128",
     embedding_function=OpenAIEmbeddings(),
 )
-print(vectorstore._collection.count())
 retriever = vectorstore.as_retriever()
-llm = ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0, max_tokens=200)
+llm = ChatOpenAI(
+    model_name="gpt-4-0125-preview", temperature=0, max_tokens=200, verbose=True
+)
 
-retrieval_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | retrieval_prompt
-    | llm
+# retrieval_chain = (
+#     {"context": retriever, "question": RunnablePassthrough()}
+#     | retrieval_prompt
+#     | llm
+#     | StrOutputParser()
+# )
+
+inputs = RunnableParallel(
+    standalone_question=RunnablePassthrough.assign(
+        chat_history=lambda x: get_buffer_string(x["chat_history"])
+    )
+    | condense_question_prompt
+    | ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0)
     | StrOutputParser()
 )
 
-resp = retrieval_chain.invoke("미궁365 보관방법은?")
-print(resp)
 
-## add chat messages
+retrieved_docs = {
+    "docs": itemgetter("standalone_question") | retriever,
+    "question": lambda x: x["standalone_question"],
+}
+
+final_inputs = {
+    "context": lambda x: combine_docs(x["docs"]),
+    "question": itemgetter("question"),
+}
+
+answer = {
+    "answer": final_inputs | retrieval_prompt | llm,
+    "docs": itemgetter("docs"),
+}
+
+conversational_retrieval_chain = inputs | retrieved_docs | answer
+
+resp = conversational_retrieval_chain.invoke(
+    {
+        "question": "미궁365 보관 방법은?",
+        "chat_history": [],
+    }
+)
+# print(resp["answer"])
+# print(resp["docs"])
--- backend/app/templates.py
+++ backend/app/templates.py
@@ -1,9 +1,30 @@
-from langchain_core.prompts import ChatPromptTemplate
+# https://python.langchain.com/docs/expression_language/cookbook/retrieval
+
+from langchain.schema import format_document
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 
 
-retrieval_template = """[질문]에 [문맥]을 바탕으로만 답변하라:
+def combine_docs(docs):
+    """
+    Combine retrieved docs into a single string w/ format_document(https://api.python.langchain.com/en/v0.0.339/schema/langchain.schema.prompt_template.format_document.html)
+    """
+    doc_prompt = PromptTemplate.from_template(template="{page_content}")
+    doc_strings = [format_document(doc, doc_prompt) for doc in docs]
+    return "\n\n".join(doc_strings)
+
+
+retrieval_template = """[질문]에 [문맥]을 근거로만 답변하라:
 [문맥] {context}
 
 [질문] {question}
 """
 retrieval_prompt = ChatPromptTemplate.from_template(retrieval_template)
+
+## Conversational Retrieval Chain
+condense_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+condense_question_prompt = PromptTemplate.from_template(condense_template)
